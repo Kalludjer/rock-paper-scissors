c1d1c301eebd842eab17329334ad5079245e89ff
Kalludjer <necicdragan3@GMAIL.COM>
2024-01-20 23:02:28 +0100

    add second part

--- script.js
+++ script.js
@@ -1,14 +1,35 @@
 //--------------------firstt part----------------------------
 //compiuter choice
 
-function getComputerChoice() { 
-    let choice = ["Rock", "Paper", "Scissors"]
-    let randomRPS = choice[Math.floor(Math.random() * choice.length)];
+function getComputerChoice() {
+    const choice = ['Rock', 'Paper', 'Scissors'];
+    const randomRPS = choice[Math.floor(Math.random() * choice.length)];
+    console.log(randomRPS);
     return randomRPS;
-}
+  }
 
 //------------------second part------------------------------
 
-function plarRaund(playerSelection, getComputerChoice){
+function playRound(playerSelection, compiuterSelection) {
+    if (playerSelection === 'Rock' && compiuterSelection === 'Scissors') {
+        return 'You Win! Rock beats Scissors';
+      } else if (playerSelection === 'Paper' && compiuterSelection === 'Rock') {
+        return 'You Win! Paper beats Rock';
+      } else if (playerSelection === 'Scissors' && compiuterSelection === 'Paper') {
+        return 'You Win! Scissors beats Paper';
+      } else if (playerSelection === 'Scissors' && compiuterSelection === 'rock') {
+        return 'You Lose! Rock beats Scissors';
+      } else if (playerSelection === 'Paper' &&  compiuterSelection === 'Scissors') {
+        return 'You Lose! Scissors beats Paper';
+      } else if (playerSelection === 'Rock' && compiuterSelection === 'Paper') {
+        return 'You Lose! Paper beats Rock';
+      } else if (playerSelection === compiuterSelection) {
+        return 'Tie! Play Another Round';
+      }
     
 }
+
+const playerSelection = "Rock";
+const compiuterSelection = getComputerChoice()
+console.log(playRound(playerSelection, compiuterSelection));   
+ 
